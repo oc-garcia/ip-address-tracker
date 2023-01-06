2146b5155edde9e232094ae8442d5ac2119c8e02
oc-garcia <octaviogarcia1988@gmail.com>
2023-01-06 14:40:45 -0300

    Advancing in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//map
 var map = L.map("map").setView([51.505, -0.09], 13);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -6,3 +7,15 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 //var marker = L.marker([51.5, -0.09]).addTo(map);
+
+ var ip = "8.8.8.8";
+ var api_key = "at_ZqrTedooSBDBHPGDG4TqhB4bBVGiR";
+ $(function () {
+   $.ajax({
+     url: "https://geo.ipify.org/api/v1",
+     data: { apiKey: api_key, ipAddress: ip },
+     success: function (data) {
+       $("body").append("<pre>" + JSON.stringify(data, "", 2) + "</pre>");
+     },
+   });
+ });
