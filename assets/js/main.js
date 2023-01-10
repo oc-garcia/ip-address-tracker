77d2443e17d390d9da0e1cfff46a06a9bcc2a75d
oc-garcia <octaviogarcia1988@gmail.com>
2023-01-10 10:48:44 -0300

    Advancing in JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,15 +8,16 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 var marker = L.marker([51.5, -0.09]).addTo(map);
 
-const country = await getCountry("8.8.8.8");
-console.log(country);
-
 async function getCountry(ipAddress) {
   const API_KEY = "at_ZqrTedooSBDBHPGDG4TqhB4bBVGiR";
   const API_ENDPOINT = "https://geo.ipify.org/api/v2/country";
   const url = `${API_ENDPOINT}?apiKey=${API_KEY}&ipAddress=${ipAddress}`;
   const response = await fetch(url);
   const data = await response.json();
+ // console.log(JSON.stringify(data));
   return data;
 }
-
+/*
+const country = await getCountry("8.8.8.8");
+console.log(country);
+*/
